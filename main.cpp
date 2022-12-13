c0ab656df3ecb585710da8fc896a8ccbddca9c2b
Mishachkld <mishachkld@gmail.com>
2022-12-13 20:48:09 +0200

    готово

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@ struct Car{
 };
 
 
-void outArray(std::vector<int> array) {
+void outArray(std::vector<int> array, int n) {
     int len = array.size();
+    std::cout << n <<"." << " " ;
     for (int i = 0; i < len; i++) {
-        std::cout << array[i] << " ";
+        std::cout << array[i] << " ";     /// по индексу достаем элемент
     }
     std::cout << std::endl;
 }
@@ -20,30 +21,29 @@ int main() {
     for (int i = 16; i > 0; i--) {
         testArray.push_back(i);                             /// добавляем в конец массива эллемент - .push_back()
     }
-    outArray(testArray);
+    outArray(testArray, 1);
     testArray.erase(testArray.begin() + 7);         /// erase Удаляет элемент или диапазон элементов в векторе из заданных позиций
-    outArray(testArray);                       // исчезла 9
+    outArray(testArray, 2);                       // исчезла 9
     testArray.insert(testArray.begin() + 7, 9);  /// insert Вставляет элемент или несколько элементов в вектор по заданному индексу
-    outArray(testArray);                       // вернулась 9
+    outArray(testArray, 3);                       // вернулась 9
     circles.emplace_back("Nissan",20);   /// emplace_back Добавляет элемент, созданный на месте (т.е. мы создаем новый объект указанного типа (здесь Car)), в конец вектора.
     testArray.emplace_back(20);                             /// Добавляет элемент, созданный на месте (т.е. мы создаем новый объект указанного типа (здесь int)), в конец вектора.
-    outArray(testArray);
+    outArray(testArray, 4);
     testArray.resize(20, 6);                     /// resize изменяет размер vector до нужного размера
-    outArray(testArray);
+    outArray(testArray, 4);
     std::cout <<testArray.capacity() << std::endl;          /// capacity показывает текущую длину хранилища, выделенного(зарезервираваного) для вектора.
-    testArray.reserve(63);                               /// reserve Изменяем размер памяти, выделеной для вектора:
+    testArray.reserve(63);                               /// reserve Изменяем размер памяти, выделеной для вектора(здесь мы увеличваем с 32 до 63):
     std::cout << testArray.capacity() << std::endl;         /// Текущая длина хранилища, выделенного вектору.
-    testArray.shrink_to_fit();
-    testArray.clear();                                      /// Очищаем vector
-    outArray(testArray);
+    testArray.shrink_to_fit();                              /// Удаляет лишнюю часть(емкость) (т.е. с 63 уменьшил до 20 (== колличество элементов)).
+    std::cout << testArray.capacity() << std::endl;
+    outArray(testArray, 5);
+    testArray[0] = 9999;                                    /// оператор присваивания
+    outArray(testArray, 5);
+    std::cout <<"Size of array: " << testArray.size() << std::endl;
+    testArray.clear();                                      /// Очищает vector (т.е. в нем ничего не лежит, он пустой)
+    std::cout << "Size of array: " << testArray.size();
+
+
 
     return 0;
 }
-
-
-
-//void random(, int n) {
-//    for (int i = 0; i < n; i++) {
-//        array->push_back(i);
-//    }
-//}
